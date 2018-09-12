fa871b2c6c64e94d3b2357c214eb64e62780fc6f
Jeongryeol <duxbellorn@gmail.com>
2018-09-12 09:21:01 +0900

    [ 18-09-17 ] Maven Project
    [ Detail ]
    - 메이븐 사용 + 트레이스

---
+++ spring_board/src/com/mvc/board/MvcMemberLogic.java
@@ -0,0 +1,20 @@
+package com.mvc.board;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MvcMemberLogic {
+	Logger logger = Logger.getLogger(MvcMemberLogic.class);
+	
+	public List<Map<String,Object>> getMemberList(Map<String,Object> pMap){
+		logger.info("MemberLogic - getMemberList");
+		//만일 게시판관리 업무에서 회원관리업무 메소드를 사용하고 싶다 어떡하지?
+		
+		return null;
+	}
+	
+}
